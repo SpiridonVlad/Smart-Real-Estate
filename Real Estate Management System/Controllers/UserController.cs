76900949a88061c71314222613a73c16d59f7504
EmilCosma <109912838+EmilCosma@users.noreply.github.com>
2024-11-03 14:26:18 +0200

    .

---
+++ Real Estate Management System/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Application.Use_Cases.Commands;
+using Application.DTOs;
+
+using Domain.Common;
+using Application.Use_Cases.Queries;
+
+namespace ToDoList.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UserController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Result<Guid>>> CreateUser([FromBody] CreateUserCommand command)
+        {
+            var result = await mediator.Send(command);
+            return CreatedAtAction(nameof(GetUserById), new { Id = result.Data }, result.Data);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers()
+        {
+            var query = new GetAllUsersQuery();
+            var result = await mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(Guid id)
+        {
+            var query = new GetUserByIdQuery { Id = id };
+            var result = await mediator.Send(query);
+            return Ok(result);
+        }
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteUser(Guid id)
+        //{
+        //    var command = new DeleteUserCommand { Id = id };
+        //    await mediator.Send(command);
+        //    return NoContent();
+        //}
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UpdateUserCommand command)
+        //{
+        //    if (id != command.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    await mediator.Send(command);
+        //    return NoContent();
+        //}
+    }
+}
