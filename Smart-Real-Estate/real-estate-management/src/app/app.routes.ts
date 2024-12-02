5633ea87afd73685a94be666591092696a130e7d
MAndrei2308 <andrei.moisa.23@gmail.com>
2024-12-02 19:01:38 +0200

    listing-create

--- Smart-Real-Estate/real-estate-management/src/app/app.routes.ts
+++ Smart-Real-Estate/real-estate-management/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { UserListComponent } from './components/user-list/user-list.component';
 import { UserCreateComponent } from './components/user-create/user-create.component';
 import { UserUpdateComponent } from './components/user-update/user-update.component';
 import { ListingListComponent } from './components/listing-list/listing-list.component';
+import { ListingCreateComponent } from './components/listing-create/listing-create.component';
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '', redirectTo: '/listings/create', pathMatch: 'full' },
   { path: 'users', component: UserListComponent },
-  {path: 'users/create', component: UserCreateComponent},
+  { path: 'users/create', component: UserCreateComponent},
   { path: 'users/update/:id', component: UserUpdateComponent },
   { path: 'listings', component: ListingListComponent },
-  // { path: 'listings/create', component: CreateListingComponent }, // Definește componenta
-  // { path: 'listings/update/:id', component: UpdateListingComponent }, // Definește componenta
+  { path: 'listings/create', component: ListingCreateComponent },
+  // { path: 'listings/update/:id', component: UpdateListingComponent }
 ];
