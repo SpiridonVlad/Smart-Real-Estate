964701ed16a1d52d885eb0e728e4d40815bac1ae
MAndrei2308 <andrei.moisa.23@gmail.com>
2024-12-02 18:28:01 +0200

    listing-list, listing.model & listing.service

--- Smart-Real-Estate/real-estate-management/src/app/app.routes.ts
+++ Smart-Real-Estate/real-estate-management/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserCreateComponent } from './components/user-create/user-create.component';
 import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { ListingListComponent } from './components/listing-list/listing-list.component';
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UserListComponent },
   {path: 'users/create', component: UserCreateComponent},
-  { path: 'users/update/:id', component: UserUpdateComponent }
+  { path: 'users/update/:id', component: UserUpdateComponent },
+  { path: 'listings', component: ListingListComponent },
+  // { path: 'listings/create', component: CreateListingComponent }, // Definește componenta
+  // { path: 'listings/update/:id', component: UpdateListingComponent }, // Definește componenta
 ];
--- Smart-Real-Estate/real-estate-management/src/app/components/listing-list/listing-list.component.ts
+++ Smart-Real-Estate/real-estate-management/src/app/components/listing-list/listing-list.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListingService } from '../../services/listing.service';
+import { Router } from '@angular/router';
+import { Listing } from '../../models/listing.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listing-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './listing-list.component.html',
-  styleUrl: './listing-list.component.css'
+  styleUrls: ['./listing-list.component.css'],
 })
-export class ListingListComponent {
+export class ListingListComponent implements OnInit {
+  listings: Listing[] = [];
+  page: number = 1;
+  pageSize: number = 5;
 
+  constructor(private listingService: ListingService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadListings();
+  }
+
+  loadListings(): void {
+    this.listingService.getPaginatedListings(this.page, this.pageSize).subscribe(
+      (response: any) => {
+        this.listings = response.data;
+      },
+      (error) => {
+        console.error('Error loading listings:', error);
+      }
+    );
+  }
+
+  navigateToCreate(): void {
+    this.router.navigate(['/listings/create']);
+  }
+
+  navigateToUpdate(listingId: string): void {
+    this.router.navigate(['/listings/update', listingId]);
+  }
 }
