4734230a672f64f519245c7d534828459b87615e
MAndrei2308 <andrei.moisa.23@gmail.com>
2024-12-02 19:32:26 +0200

    listing-update

--- Smart-Real-Estate/real-estate-management/src/app/app.routes.ts
+++ Smart-Real-Estate/real-estate-management/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserCreateComponent } from './components/user-create/user-create.compon
 import { UserUpdateComponent } from './components/user-update/user-update.component';
 import { ListingListComponent } from './components/listing-list/listing-list.component';
 import { ListingCreateComponent } from './components/listing-create/listing-create.component';
+import { ListingUpdateComponent } from './components/listing-update/listing-update.component';
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/listings/create', pathMatch: 'full' },
   { path: 'users', component: UserListComponent },
@@ -11,5 +12,5 @@ export const appRoutes: Routes = [
   { path: 'users/update/:id', component: UserUpdateComponent },
   { path: 'listings', component: ListingListComponent },
   { path: 'listings/create', component: ListingCreateComponent },
-  // { path: 'listings/update/:id', component: UpdateListingComponent }
+  { path: 'listings/update/:id', component: ListingUpdateComponent }
 ];
