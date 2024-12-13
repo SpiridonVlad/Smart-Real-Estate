c5bdd436b3f85f3c8b2f6e3c5edfda5e5dd22d83
Spiridon Vlad-Iustin <iustin.spiridon@gmail.com>
2024-12-13 10:53:12 +0200

    [Spiridon Vlad] Email for company, plans

---
+++ Smart-Real-Estate/Src/WebAPI/Controllers/ActionControllers/CompareController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Real_Estate_Management_System.Controllers.ActionControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompareController : ControllerBase
+    {
+    }
+}
