10edd6dc271f00c67bfb50baa3cfa45b9539be6c
MAndrei2308 <andrei.moisa.23@gmail.com>
2024-12-15 04:52:09 +0200

    made records page prettier and added filter popup

--- Smart-Real-Estate/real-estate-management/src/app/components/record-list/record-list.component.ts
+++ Smart-Real-Estate/real-estate-management/src/app/components/record-list/record-list.component.ts
@@ -16,6 +16,10 @@ export class RecordListComponent implements OnInit {
   pageSize: number = 5;
   pages: number[] = [1, 2, 3, 4, 5];
   pageSizes: number[] = [5, 10, 15];
+  showFilterPopup: boolean = false;
+  pTypes: string[] = ['Apartment', 'Office', 'Studio', 'CommercialSpace', 'House', 'Garage'];
+  cities: string[] = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose'];
+  filter: { pType: string; city: string ; minPrice: number; maxPrice: number} = { pType: '', city: '', minPrice: 0, maxPrice: 999999 };
 
   constructor(private recordService: RecordService) {}
 
@@ -36,5 +40,27 @@ export class RecordListComponent implements OnInit {
 
   getFeatures(features: { [key: string]: number }): { key: string; value: number }[] {
     return Object.entries(features).map(([key, value]) => ({ key, value }));
+  }
+
+  toggleFilterPopup(): void {
+    this.showFilterPopup = !this.showFilterPopup;
+  }
+
+  applyFilters(): void {
+    this.loadRecords();
+    this.showFilterPopup = false;
+  }
+
+  nextPage(): void {
+    if (this.page < this.pages.length) {
+      this.page++;
+      this.loadRecords();
+    }
+  }
+  previousPage(): void {
+    if (this.page > 1) {
+      this.page--;
+      this.loadRecords();
+    }
   }
 }
