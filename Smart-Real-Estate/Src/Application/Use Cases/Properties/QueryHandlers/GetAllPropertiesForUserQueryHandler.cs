57cd4d97d8ee51d502929be41e61ce0daaf2c5cc
Spiridon Vlad-Iustin <iustin.spiridon@gmail.com>
2025-01-11 23:53:32 +0200

    [Spiridon Vlad] small change

--- Smart-Real-Estate/Src/Application/Use Cases/Properties/QueryHandlers/GetAllPropertiesForUserQueryHandler.cs
+++ Smart-Real-Estate/Src/Application/Use Cases/Properties/QueryHandlers/GetAllPropertiesForUserQueryHandler.cs
@@ -2,21 +2,36 @@
 using Application.Use_Cases.Property.Queries;
 using AutoMapper;
 using Domain.Common;
+using Domain.Entities;
 using Domain.Repositories;
 using MediatR;
 
 namespace Application.Use_Cases.Property.QueryHandlers
 {
-    public class GetAllPropertiesForUserQueryHandler(IMapper mapper, IPropertyRepository propertyRepository) : IRequestHandler<GetAllPropertiesForUserQuery, Result<IEnumerable<PropertyDto>>>
+    public class GetAllPropertiesForUserQueryHandler(IMapper mapper, IPropertyRepository propertyRepository,IAddressRepository addressRepository) : IRequestHandler<GetAllPropertiesForUserQuery, Result<IEnumerable<PropertyDto>>>
     {
         private readonly IMapper mapper = mapper;
         private readonly IPropertyRepository propertyRepository = propertyRepository;
+        private readonly IAddressRepository addressRepository = addressRepository;
         public async Task<Result<IEnumerable<PropertyDto>>> Handle(GetAllPropertiesForUserQuery request, CancellationToken cancellationToken)
         {
             var properties = await propertyRepository.GetAllForUserAsync(request.UserId);
+
+            List<PropertyDto> propertyDtos = [];
+
             if (properties.IsSuccess)
             {
-                return Result<IEnumerable<PropertyDto>>.Success(mapper.Map<IEnumerable<PropertyDto>>(properties.Data));
+                foreach (var property in properties.Data)
+                {
+                    var address = await addressRepository.GetByIdAsync(property.AddressId);
+                    if (!address.IsSuccess)
+                    {
+                        return Result<IEnumerable<PropertyDto>>.Failure("Address not found.");
+                    }
+                    property.Address = address.Data;
+                    propertyDtos.Add(mapper.Map<PropertyDto>(property));
+                }
+                return Result<IEnumerable<PropertyDto>>.Success(mapper.Map<IEnumerable<PropertyDto>>(propertyDtos));
             }
             return Result<IEnumerable<PropertyDto>>.Failure("Properties not found");
         }
